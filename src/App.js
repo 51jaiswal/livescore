c98f523840e93ebf86e675b4597ba6a364ceae0e
51jaiswal <37514555+51jaiswal@users.noreply.github.com>
2021-06-06 12:14:00 +0530

    theme

--- src/App.js
+++ src/App.js
@@ -1,69 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
-import React,{Fragment,useEffect,useState} from "react"
-import { Button, Grid, Typography } from '@material-ui/core';
-import Navbar from "./Components/Navbar"
-import MyCard from './Components/MyCard';
-import {getMatches} from "./Api/Api"
-import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
-import purple from '@material-ui/core/colors/purple';
-import green from '@material-ui/core/colors/green';
-
-const theme = createMuiTheme({
-  palette: {
-    primary: {
-      main: purple[500],
-    },
-    secondary: {
-      main: green[500],
-    },
-  },
-})
+import logo from "./logo.svg";
+import "./App.css";
+import React, { Fragment, useEffect, useState } from "react";
+import { Button, Grid, Paper, Typography } from "@material-ui/core";
+import Navbar from "./Components/Navbar";
+import MyCard from "./Components/MyCard";
+import { getMatches } from "./Api/Api";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
+import purple from "@material-ui/core/colors/purple";
+import green from "@material-ui/core/colors/green";
 
 function App() {
+  const [matches, setMatches] = useState([]);
+  const [darkmode,setDarkmode] = useState(false)
 
   const theme = createMuiTheme({
     palette: {
-      primary: {
-        main: '#1b5e20',
-      },
-      secondary: {
-        main:'#1b5e20',
-      },
+      type:darkmode? "dark":"light",
     },
-  })
-
-  const [matches,setMatches] = useState([]);
+  });
   useEffect(() => {
-    {getMatches().then((data)=>setMatches(data.matches))
-    .catch(err=>alert("couldn't load data"))}
-  }, [])
-  return (
-    
-    <div className="App">
-      <Navbar/>
-      <Typography variant="h4" styele={{margin:"20px"}}>Welcome to My live score App</Typography>
-        <ThemeProvider theme={theme}>
+    {
+      getMatches()
+        .then((data) => setMatches(data.matches))
+        .catch((err) => alert("couldn't load data"));
+    }
+  }, []);
 
-        
-      <Grid container>
-        <Grid  md="2"></Grid>
-        <Grid  md="8">
-           {
-          matches.map((match)=>(
-            <Fragment key={match.unique_id}>
-              {
-                match.type=="Twenty20" ?( <MyCard key={match.unique_id} match={match}/>):""
-              }
-            </Fragment>
-          
-          ))
-        }
-        </Grid>
-      </Grid>
-       </ThemeProvider>
-        
-        {/* <Button variant="contained" color="outlined">Click</Button> */}
+  const dark = (e)=>{
+    console.log(darkmode)
+    setDarkmode(!darkmode)
+  }
+
+  return (
+    <div className="App">
+      <ThemeProvider theme={theme}>
+        <Paper>      
+        <Navbar dark={dark} />
+        <Typography variant="h4" style={{ margin: "20px" }}>
+          Welcome to My live score App
+        </Typography>
+
+        <Grid container>
+          <Grid md="2"></Grid>
+          <Grid md="8">
+            {matches.map((match) => (
+              <Fragment key={match.unique_id}>
+                {match.type == "Twenty20" ? (
+                  <MyCard key={match.unique_id} match={match} />
+                ) : (
+                  ""
+                )}
+              </Fragment>
+            ))}
+          </Grid>
+        </Grid></Paper>
+      </ThemeProvider>
+
+      {/* <Button variant="contained" color="outlined">Click</Button> */}
     </div>
   );
 }
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,9 +1,9 @@
-import { AppBar, IconButton, Toolbar, Typography } from "@material-ui/core"
+import { AppBar, IconButton, Switch, Toolbar, Typography } from "@material-ui/core"
 import { LiveTv } from "@material-ui/icons"
 import MenuIcon from "@material-ui/icons/Menu"
 import react, {Component} from "react"
-
-const Navbar=()=>{
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+const Navbar=(props)=>{
 
     return (
         <AppBar position="static">
@@ -11,8 +11,14 @@ const Navbar=()=>{
                 <IconButton color="inherit">
                     {/* <MenuIcon/> */}
                     <LiveTv/>
+                  
                 </IconButton>
-                <Typography variant="h6">Live Score</Typography>
+                <Typography variant="h6">Live Score</Typography>   
+               <IconButton style={{float:"right"}} color="black" edge="end">
+                    <Switch alignItems="end" onChange={props.dark} ></Switch>
+               </IconButton>
+               
+    
             </Toolbar>
         </AppBar>
     )
